225c58d2bbc907bd508a396d990fd75c3de4edde
ferdyhape <ferdyhahan5@gmail.com>
2022-04-02 04:08:39 +0700

    Add page Login and Register

---
+++ resources/views/guest_side/login.blade.php
@@ -0,0 +1,21 @@
+@extends('guest_side.layouts.main')
+@section('content')
+<div class="header home">
+    <div class="container-fluid">
+        <div class="hero row align-items-center">
+            <div class="container" id="containerlogin">
+                <img src="/img/logo_technician.png" class="mx-auto d-block">
+                <form action="#" class="form-login" method="POST" autocomplete="off">
+                    <input class="form-control shadow-sm" name="username" id="username" type="text" placeholder="Username">
+                    <input class="form-control shadow-sm" name="password" id="password" type="password" placeholder="Password">
+                    <button type="submit" class="btn btn-success shadow-sm border-0">Login</button>
+                </form>
+                <div class="d-flex justify-content-between text-bawah">
+                    <div class="mt-2"><a href="/register_">Register</a></div>
+                    <div class="mt-2"><a href="#">Forgot Password?</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
