44814cf04f469c70541c793b786e421e984e65b7
Ilham-sinatrio20 <sinatrio20@gmail.com>
2022-04-03 20:11:50 +0700

    Install Livewire

--- resources/views/guest_side/layouts/main.blade.php
+++ resources/views/guest_side/layouts/main.blade.php
@@ -9,9 +9,6 @@
     <meta content="Cleaning Company Website Template" name="keywords">
     <meta content="Cleaning Company Website Template" name="description">
 
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('index');
+Route::prefix('/')->group(function(){
+    Route::get('index', function () {return view('index');});
+    Route::get('contact', function () {return view('contact');});
+    Route::get('about', function () {return view('about');});
+    Route::get('/service', function () {return view('service');});
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
 
-Route::get('/about', function () {
-    return view('about');
-});
 
 Route::get('/penggunaanTemplate', function () {
     return view('guest_side.penggunaanTemplateEngine', [
