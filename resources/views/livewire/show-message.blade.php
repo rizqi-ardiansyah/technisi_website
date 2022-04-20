2988c8c1e7b5cff68570d4436e39ce527d56e76c
rizqi-ardiansyah <86498942+rizqi-ardiansyah@users.noreply.github.com>
2022-04-20 07:34:05 +0700

    Delete resources directory

--- resources/views/livewire/show-message.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container-fluid">
-        <div class="container">
-            @livewire('show-message')
-        </div>
-    </div>
-@endsection
